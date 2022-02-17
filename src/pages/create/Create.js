8e4b36fbf9fad73342033879b670e6c53e6c4673
emilien-wittchen <abizw97@gmail.com>
2022-02-17 18:08:40 +0100

    real-time data collection, updating data, deleting data

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,9 @@
 import './Create.css';
 import {useEffect, useState} from 'react';
 import {useRef} from 'react';
-import {useFetch} from '../../hooks/useFetch';
+// import {useFetch} from '../../hooks/useFetch';
 import {useHistory} from 'react-router-dom';
+import {projectFirestore} from '../../firebase/config';
 
 export default function Create() {
   const [title, setTitle] = useState('');
@@ -13,19 +14,26 @@ export default function Create() {
   const ingredientInput = useRef(null);
   const history = useHistory();
 
-  const {postData, data, error} = useFetch(
-    'http://localhost:3000/recipes',
-    'POST'
-  );
+  // const {postData, data, error} = useFetch(
+  //   'http://localhost:3000/recipes',
+  //   'POST'
+  // );
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    postData({
+    const doc = {
       title,
       ingredients,
       method,
       cookingTime: cookingTime + ' minutes',
-    });
+    };
+
+    try {
+      await projectFirestore.collection('recipes').add(doc);
+      history.push('/');
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleAdd = (e) => {
@@ -39,12 +47,12 @@ export default function Create() {
     ingredientInput.current.focus();
   };
 
-  // redirect the user after data response
-  useEffect(() => {
-    if (data) {
-      history.push('/');
-    }
-  }, [data]);
+  // redirect the use r after data response
+  // useEffect(() => {
+  //   if (data) {
+  //     history.push('/');
+  //   }
+  // }, [data]);
 
   return (
     <div className='create'>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,10 +12,8 @@ export default function Home() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
-      .collection('recipes')
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection('recipes').onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError('No recipes to load');
           setIsPending(false);
@@ -27,11 +25,13 @@ export default function Home() {
           setData(results);
           setIsPending(false);
         }
-      })
-      .catch((err) => {
+      },
+      (err) => {
         setError(err.message);
         setIsPending(false);
-      });
+      }
+    );
+    return () => unsub();
   }, []);
 
   return (
